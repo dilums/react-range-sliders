8d9aeaccb317ba9106dccd26be6227ff46e53639
dilums <dilumsanjaya@gmail.com>
2023-06-15 19:47:21 +0530

    initialize next.js  repo

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from "@/components/Head";
+
+export default function Home() {
+  return (
+    <>
+      <Head title="React Range Sliders" slug="/" />
+      <div className=""> Home Page </div>
+    </>
+  );
+}
